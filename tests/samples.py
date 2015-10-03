f6f47bfa8823a03c8908aaa7377a926e8ac76503
Jeff Nagasuga <jeffrey.nagasuga@disney.com>
2015-10-02 17:53:35 -0700

    refactoring test

---
+++ tests/samples.py
@@ -0,0 +1,23 @@
+def sample(arg1, arg2, kwargs1='one', kwargs2=None):
+    """This is sample docstring.
+
+    This is the detail information about this function. This could be
+    pretty long and span multiline/multi paragraph.
+
+    Continuing the detail information about this function. Combine with
+    the above paragraph for full explanation of what this function is.
+    """
+    pass
+
+
+def sample_2(arg1, arg2, kwargs1='one', kwargs2=None):
+    """This is sample docstring that is long that will go multiline
+    and continues here and ... on and on.
+
+    This is the detail information about this function. This could be
+    pretty long and span multiline/multi paragraph.
+
+    Continuing the detail information about this function. Combine with
+    the above paragraph for full explanation of what this function is.
+    """
+    pass
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,31 +1,7 @@
 from unittest import TestCase
 
 import docpy.parser
-
-
-def sample(arg1, arg2, kwargs1='one', kwargs2=None):
-    """This is sample docstring.
-
-    This is the detail information about this function. This could be
-    pretty long and span multiline/multi paragraph.
-
-    Continuing the detail information about this function. Combine with
-    the above paragraph for full explanation of what this function is.
-    """
-    pass
-
-
-def sample_2(arg1, arg2, kwargs1='one', kwargs2=None):
-    """This is sample docstring that is long that will go multiline
-    and continues here and ... on and on.
-
-    This is the detail information about this function. This could be
-    pretty long and span multiline/multi paragraph.
-
-    Continuing the detail information about this function. Combine with
-    the above paragraph for full explanation of what this function is.
-    """
-    pass
+from .samples import sample
 
 
 class ParseTest(TestCase):
@@ -34,10 +10,10 @@ class ParseTest(TestCase):
         self.assertEqual(res['name'], 'sample')
         self.assertEqual(res['summary'], 'This is sample docstring.')
         exp_desc = ('This is the detail information about this function. '
-                      'This could be pretty long and span multiline/multi '
-                      'paragraph.\n\nContinuing the detail information about '
-                      'this function. Combine with the above paragraph for '
-                      'full explanation of what this function is.')
+                    'This could be pretty long and span multiline/multi '
+                    'paragraph.\n\nContinuing the detail information about '
+                    'this function. Combine with the above paragraph for '
+                    'full explanation of what this function is.')
         self.assertEqual(res['description'], exp_desc)
         exp_params = [{'type': 'arg', 'name': 'arg1'},
                       {'type': 'arg', 'name': 'arg2'},
@@ -47,51 +23,54 @@ class ParseTest(TestCase):
 
 
 class ParseDocstringTest(TestCase):
-    def test_no_docstring(self):
+    @staticmethod
+    def generate_docstring(summary=None, description=None):
         docstring = ''
+        if summary:
+            docstring = """{}""".format(summary)
+        if description:
+            docstring += '\n\n{}'.format(description)
+        return docstring
+
+    def assert_parse(self, docstring, summary, description):
         res = docpy.parser.parse_docstring(docstring=docstring)
-        self.assertEqual(res['summary'], None)
-        self.assertEqual(res['description'], None)
+        self.assertEqual(res['summary'], summary)
+        self.assertEqual(res['description'], description)
+
+    def test_no_docstring(self):
+        docstring = self.generate_docstring()
+        self.assert_parse(docstring, summary=None, description=None)
 
     def test_simple_summary(self):
         summary = 'This is simple docstring.'
-        docstring = """%s""" % summary
-        res = docpy.parser.parse_docstring(docstring=docstring)
-        self.assertEqual(res['summary'], summary)
-        self.assertEqual(res['description'], None)
+        docstring = self.generate_docstring(summary=summary)
+        self.assert_parse(docstring, summary=summary, description=None)
 
     def test_multiline_summary(self):
-        docstring = """This is sample docstring that is long that will go 
+        summary = """This is sample docstring that is long that will go 
                     multiline and continues here and ... on and on.
                     """
-        res = docpy.parser.parse_docstring(docstring=docstring)
+        docstring = self.generate_docstring(summary=summary)
         exp_summary = ('This is sample docstring that is long that will go '
                        'multiline and continues here and ... on and on.')
-        self.assertEqual(res['summary'], exp_summary)
-        self.assertEqual(res['description'], None)
+        self.assert_parse(docstring, summary=exp_summary, description=None)
 
     def test_simple_summary_simple_description(self):
         summary = 'This is simple docstring.'
         description = 'This is simple description.'
-        docstring = """%s
-
-                    %s""" % (summary, description)
-        res = docpy.parser.parse_docstring(docstring=docstring)
-        self.assertEqual(res['summary'], summary)
-        self.assertEqual(res['description'], description)
+        docstring = self.generate_docstring(summary=summary,
+                                            description=description)
+        self.assert_parse(docstring, summary=summary, description=description)
 
     def test_simple_summary_multiline_description(self):
         summary = 'This is simple docstring.'
         description = """This is simple description. This description will
                       span over multiple lines."""
-        docstring = """%s
-
-                    %s""" % (summary, description)
-        res = docpy.parser.parse_docstring(docstring=docstring)
-        self.assertEqual(res['summary'], summary)
+        docstring = self.generate_docstring(summary=summary,
+                                            description=description)
         exp_desc = ('This is simple description. This description will span '
                     'over multiple lines.')
-        self.assertEqual(res['description'], exp_desc)
+        self.assert_parse(docstring, summary=summary, description=exp_desc)
 
     def test_simple_summary_multiparagraph_description(self):
         summary = 'This is simple docstring.'
@@ -101,16 +80,13 @@ class ParseDocstringTest(TestCase):
                       Also, this description will have multiple paragraphs
                       to make things a bit more interesting...
                       """
-        docstring = """%s
-
-                    %s""" % (summary, description)
-        res = docpy.parser.parse_docstring(docstring=docstring)
-        self.assertEqual(res['summary'], summary)
+        docstring = self.generate_docstring(summary=summary,
+                                            description=description)
         exp_desc = ('This is simple description. This description will span '
                     'over multiple lines.\n\nAlso, this description will have '
                     'multiple paragraphs to make things a bit more '
                     'interesting...')
-        self.assertEqual(res['description'], exp_desc)
+        self.assert_parse(docstring, summary=summary, description=exp_desc)
 
     def test_multiline_summary_multiparagraph_description(self):
         summary = """This is sample docstring that is long that will go 
@@ -122,15 +98,12 @@ class ParseDocstringTest(TestCase):
                       Also, this description will have multiple paragraphs
                       to make things a bit more interesting...
                       """
-        docstring = """%s
-
-                    %s""" % (summary, description)
-        res = docpy.parser.parse_docstring(docstring=docstring)
+        docstring = self.generate_docstring(summary=summary,
+                                            description=description)
         exp_summary = ('This is sample docstring that is long that will go '
                        'multiline and continues here and ... on and on.')
-        self.assertEqual(res['summary'], exp_summary)
         exp_desc = ('This is simple description. This description will span '
                     'over multiple lines.\n\nAlso, this description will have '
                     'multiple paragraphs to make things a bit more '
                     'interesting...')
-        self.assertEqual(res['description'], exp_desc)
+        self.assert_parse(docstring, summary=exp_summary, description=exp_desc)
