32874ccf01f6f185ba346fa73a3d35b8df92f75b
Jeff Nagasuga <detpulin.dev@gmail.com>
2015-10-07 23:01:13 -0700

    v0.1.2

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name='docpy',
-    version='0.1.1',
+    version='0.1.2',
     packages=['docpy'],
     install_requires=[
         'jinja2',
         'pyyaml',
     ],
+    tests_require=[
+        'pytest',
+        'six',
+    ],
+    test_suite='py.test',
     dependency_links=[],
     description='Python documentation generator using docstrings',
     author='Jeff Nagasuga',
